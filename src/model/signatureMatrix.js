24cfe4b7170673f21a2b8f8ee2c0c654fd02612d
Jonathan Carter <joncart@microsoft.com>
2018-11-20 10:45:45 -0800

    Fixing bonus cells

--- src/model/signatureMatrix.js
+++ src/model/signatureMatrix.js
@@ -1,5 +1,6 @@
 import { isBonusCell, isSpecialCell } from "./bonusCells";
 import { signatures } from "./signatures";
+import { observable } from "mobx";
 
 const COLUMNS = 30;
 const ROWS = 20;
@@ -8,12 +9,12 @@ const matrix = [];
 for (let x = 0; x < COLUMNS; x++) {
   for (let y = 0; y < ROWS; y++) {
     const cellNumber = y + (x * ROWS) + 1;
-    const isBonus = isBonusCell(cellNumber);
+    const isBonus = isBonusCell(cellNumber.toString());
     const isSpecial = isSpecialCell(cellNumber);
 
-    const signature = signatures.unshift();
+    const signature = signatures.shift();
     matrix.push({ isBonus, isSpecial, signature });
   }
 }
 
-export default matrix;
+export default observable(matrix);
